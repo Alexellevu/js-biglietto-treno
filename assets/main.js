5e1f152b823a9927cf8cf63d6fc2e674b8a3ba77
alexellevu <newnapster83@gmail.com>
2021-04-23 16:57:34 +0200

    creazione variabili per km età e sconti

---
+++ assets/main.js
@@ -0,0 +1,21 @@
+//chiediamo all' utente i km da fare e l 'età
+var numKm = parseInt(prompt('inserisci i km del viaggio'));
+console.log(numKm);
+var età = parseInt(prompt('inserisci la tua età'));
+console.log(età);
+
+//calcoliamo i km moltiplicando i km inseriti dall'utente per 0,21.
+var prezzo = numKm * 0.21;
+console.log(prezzo);
+
+//variabili con lo sconto per i minorenni
+var sconto_min = ((prezzo*20)/100);
+var prezzo_min = prezzo - sconto_min;
+console.log(prezzo_min);
+
+//variabili con lo sconto per gli anziani
+var sconto_anziani = ((prezzo*40)/100);
+var prezzo_anziani = prezzo - sconto_anziani;
+console.log(prezzo_anziani);
+
+
